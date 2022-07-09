48e7acb2089ba0a9e8b91efa53d58bf5f47f9be7
hdyaro77 <harshit.dubey1923@gmail.com>
2022-07-09 17:13:40 +0530

    ready to deploy

--- src/Home/FetchData.js
+++ src/Home/FetchData.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FilterData } from "./FilterData";
+
 export const fetchData = async (query, loadingHandler) => {
   var temp = query;
   if (temp.charAt(0) === "@") temp = temp.substring(1);
@@ -10,13 +11,13 @@ export const fetchData = async (query, loadingHandler) => {
       url: "https://instagram-bulk-profile-scrapper.p.rapidapi.com/clients/api/ig/ig_profile",
       params: { ig: temp, response_type: "long", corsEnabled: "true" },
       headers: {
-        "X-RapidAPI-Key": "33fd4a085emsh031c5f985765781p14c1a5jsn19d7125dcb86",
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
         "X-RapidAPI-Host": "instagram-bulk-profile-scrapper.p.rapidapi.com",
       },
     };
     const res = await axios.request(options);
     if (res) {
-        console.log(res.data[0]);
+      console.log(res.data[0]);
       const data = FilterData(res?.data[0]);
       return data;
     }
